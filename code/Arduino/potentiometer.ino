3598edb72a61612514c03176ac92bbca554994c6
bottlecoder-raj <hazrarajnarayan55@gmail.com>
2026-06-11 12:34:18 +0530

    led volume

--- code/Arduino/potentiometer.ino
+++ code/Arduino/potentiometer.ino
@@ -1,5 +1,4 @@
-#define potPin 34
-
+// Define the pin connected to the internal LED
 #define L1 16
 #define L2 17
 #define L3 25
@@ -8,28 +7,42 @@
 #define L6 5
 #define L7 18
 #define L8 19
-
-int leds[] = {L1, L2, L3, L4, L5, L6, L7, L8};
-
+int arr[8]={L1,L2,L3,L4,L5,L6,L7,L8};
 void setup() {
-  Serial.begin(115200);
+  // Initialize the LED pin as an output
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L3, OUTPUT);
+  pinMode(L4, OUTPUT);
+  pinMode(L5, OUTPUT);
+  pinMode(L6, OUTPUT);
+  pinMode(L7, OUTPUT);
+  pinMode(L8, OUTPUT);
+  digitalWrite(L1, HIGH);   
+  digitalWrite(L2, HIGH);   
+  digitalWrite(L3, HIGH);   
+  digitalWrite(L4, HIGH);   
+  digitalWrite(L5, HIGH);   
+  digitalWrite(L6, HIGH);   
+  digitalWrite(L7, HIGH);   
+  digitalWrite(L8, HIGH);   
+  Serial.begin(9600);
 
-  for (int i = 0; i < 8; i++) {
-    pinMode(leds[i], OUTPUT);
-  }
 }
+void loop(){
 
-void loop() {
-  int potValue = analogRead(potPin);
+   for(int i=0;i<=8;i++){
+      digitalWrite(arr[i],HIGH);
+    }
+    int pot = analogRead(33);
+    pot= map(pot,0,4096,0,80);
+    pot=pot/10;
+    for(int i=0;i<=pot;i++){
+      digitalWrite(arr[i],LOW);
+    }
 
-  Serial.print("Potentiometer Value: ");
-  Serial.println(potValue);
+Serial.println();
+  // }
 
-  int level = map(potValue, 0, 4095, 0, 8);
-
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(leds[i], (i < level) ? LOW : HIGH);
-  }
-
-  delay(50);
+  
 }
